100d79f9298f99e17285b8fef62f66eec41b8144
szheron <heronbr7@gmail.com>
2023-04-28 09:41:27 -0300

    feat: layout da tela cadastrar finalizado

--- src/components/CustomButton.js
+++ src/components/CustomButton.js
@@ -1,6 +1,6 @@
 export default function CustomButton({children}){
     return (
-        <button className="bg-blue-500 p-3 rounded-md text-white">
+        <button className="bg-blue-500 font-bold p-3 rounded-md text-white">
             {children}
         </button>
     )
