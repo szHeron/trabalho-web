41ef42d0a5a14a1649e1ee8bf93d3797004009b8
szheron <heronbr7@gmail.com>
2023-05-19 15:58:14 -0300

    feat: added header

---
+++ src/components/Header.js
@@ -0,0 +1,12 @@
+export default function Header(){
+    return (
+        <div className="flex flex-row justify-between w-screen p-4 shadow-md">
+            <p className="font-bold">
+                Ajuda aí
+            </p>
+            <button>
+                Entrar
+            </button>
+        </div>
+    )
+}
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,19 +1,10 @@
 import Link from "next/link"
+import Header from "@/components/Header"
 
 export default function Home() {
   return (
     <div>
-    <ul>
-      <li>
-        <Link href="/">Home</Link>
-      </li>
-      <li>
-        <Link href="/signin">Entrar</Link>
-      </li>
-      <li>
-        <Link href="/signup">Cadastrar</Link>
-      </li>
-    </ul>
+      <Header/>
     </div>
   )
 }
