614076965516b649b32d0186f80f6871c54a783b
szheron <heronbr7@gmail.com>
2023-05-20 15:23:46 -0300

    feat: added basic topic layout

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,16 @@
 import Link from "next/link"
 import Header from "@/components/Header"
+import QuestCard from "@/components/QuestCard"
 
 export default function Home() {
   return (
     <div>
       <Header/>
+      <div className="flex flex-col gap-4 items-center pt-4 pb-4">
+        <QuestCard/>
+        <QuestCard/>
+        <QuestCard/>
+      </div>
     </div>
   )
 }
