7c967642e1644f4642867884563a4fb291493912
szheron <heronbr7@gmail.com>
2023-05-22 11:05:36 -0300

    feat: added link to signin

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,12 +1,19 @@
+import Link from "next/link"
+
 export default function Header(){
     return (
         <div className="flex flex-row bg-[#2f80ed] justify-between items-center w-screen p-3 shadow-md">
             <p className="font-bold text-white">
                 FORUFC
             </p>
-            <button className="bg-white font-bold text-[#2f80ed] p-2 px-6 rounded-lg mr-8">
+            <Link
+                className="bg-white font-bold text-[#2f80ed] p-2 px-6 rounded-lg mr-8"
+                href={{
+                    pathname: '/signin'
+                }}
+            >
                 Entrar
-            </button>
+            </Link>
         </div>
     )
 }
