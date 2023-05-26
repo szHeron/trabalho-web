b746496e07eae8e0e4769bfd93ab0ab65dc22acb
szheron <heronbr7@gmail.com>
2023-05-26 15:10:29 -0300

    refactor: trocado nome do componente e adc link

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,15 +1,14 @@
-import Link from "next/link"
 import Header from "@/components/Header"
-import QuestCard from "@/components/QuestCard"
+import CommentCard from "@/components/CommentCard"
 
 export default function Home() {
   return (
     <div className=" overflow-x-hidden">
       <Header/>
       <div className="flex flex-col gap-4 items-center pt-4 pb-4">
-        <QuestCard/>
-        <QuestCard/>
-        <QuestCard/>
+        <CommentCard/>
+        <CommentCard/>
+        <CommentCard/>
       </div>
     </div>
   )
