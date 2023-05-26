81b76cd7bd931890978dfeb80e5e54bd51e16752
szheron <heronbr7@gmail.com>
2023-05-26 15:12:11 -0300

    feat: added basic layout

---
+++ src/pages/comment.js
@@ -0,0 +1,11 @@
+import CommentCard from "@/components/CommentCard"
+import Header from "@/components/Header"
+
+export default function Comment() {
+  return (
+    <div>
+        <Header/>
+        <CommentCard/>
+    </div>
+  )
+}
